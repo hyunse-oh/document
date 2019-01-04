4407915ff3686db00f830bc72b7d9651e7106a9b
hyunse-oh <xpardon@gmail.com>
2019-01-04 13:34:57 +0900

    test2

--- createPdf/src/main.java
+++ createPdf/src/main.java
@@ -5,6 +5,8 @@ import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.PDFontSetting;
 
+import createPdf.com.pdf.document.domain.PDFConfig;
+
 public class main {
 	public static void main(String[]arg) throws Exception{
 		PDDocument pDoc = new PDDocument();
@@ -17,10 +19,17 @@ public class main {
 
 		PDPageContentStream pcs = new PDPageContentStream(pDoc, page);
 
+		PDFConfig config = new PDFConfig();
+		config.setFont(PDType1Font.TIMES_BOLD);
+		config.setFontSize(12);
+		config.setPositionX(100);
+		config.setPositionY(700);
+		config.setText("Hello~~~~");
+
 		pcs.beginText();
-		pcs.setFont(PDType1Font.TIMES_BOLD, 12);
-		pcs.newLineAtOffset(100, 700);
-		pcs.showText("1234");
+		pcs.setFont(config.getFont(), config.getFontSize());
+		pcs.newLineAtOffset(config.getPositionX(), config.getPositionY());
+		pcs.showText(config.getText());
 		pcs.endText();
 		pcs.close();
 
